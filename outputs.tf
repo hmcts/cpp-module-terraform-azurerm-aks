70cef0a99ecc75c6d428dabcb92546034e196d84
varunvijayagopal <105358638+varunvijayagopal@users.noreply.github.com>
2023-03-20 11:27:28 +0000

    Dev/aks upgrade (#5)
    * bump azurerm version
    
    * bump azurerm version
    
    * remove output
    
    * remove output
    
    * remove output
    
    * remove output
    
    * remove output
    
    * remove output
    
    * remove output
    
    * remove output

--- outputs.tf
+++ outputs.tf
@@ -1,26 +1,26 @@
-output "client_key" {
-  value = azurerm_kubernetes_cluster.main.kube_config[0].client_key
-}
-
-output "client_certificate" {
-  value = azurerm_kubernetes_cluster.main.kube_config[0].client_certificate
-}
-
-output "cluster_ca_certificate" {
-  value = azurerm_kubernetes_cluster.main.kube_config[0].cluster_ca_certificate
-}
-
-output "host" {
-  value = azurerm_kubernetes_cluster.main.kube_config[0].host
-}
-
-output "username" {
-  value = azurerm_kubernetes_cluster.main.kube_config[0].username
-}
-
-output "password" {
-  value = azurerm_kubernetes_cluster.main.kube_config[0].password
-}
+#output "client_key" {
+#  value = azurerm_kubernetes_cluster.main.kube_config[0].client_key
+#}
+#
+#output "client_certificate" {
+#  value = azurerm_kubernetes_cluster.main.kube_config[0].client_certificate
+#}
+#
+#output "cluster_ca_certificate" {
+#  value = azurerm_kubernetes_cluster.main.kube_config[0].cluster_ca_certificate
+#}
+#
+#output "host" {
+#  value = azurerm_kubernetes_cluster.main.kube_config[0].host
+#}
+#
+#output "username" {
+#  value = azurerm_kubernetes_cluster.main.kube_config[0].username
+#}
+#
+#output "password" {
+#  value = azurerm_kubernetes_cluster.main.kube_config[0].password
+#}
 
 output "node_resource_group" {
   value = azurerm_kubernetes_cluster.main.node_resource_group
