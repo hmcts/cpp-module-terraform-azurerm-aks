c5dde5c59667eaf840f2be92d946f1531af4995e
varunvijayagopal <varun.vijayagopal@hmcts.net>
2023-08-17 10:42:16 +0100

    remove output

---
+++ tests/terratest/fixture/outputs.tf
@@ -0,0 +1,24 @@
+output "test_aks_id" {
+  value = module.aks.aks_id
+}
+
+output "test_admin_host" {
+  value = module.aks.admin_host
+}
+
+output "test_kube_raw" {
+  sensitive = true
+  value     = module.aks.kube_config_raw
+}
+
+output "random_id" {
+  value = random_id.prefix.hex
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.aks.name
+}
+
+output "subscription_id" {
+  value = data.azurerm_subscription.current.subscription_id
+}
