160a46399493a2db374408e3f04ef2d6ff538fad
salim abdulkareem <salim.abdulkareem@justice.gov.uk>
2025-01-31 16:32:53 +0000

    add feature

--- tests/terratest/fixture/main.tf
+++ tests/terratest/fixture/main.tf
@@ -1,12 +1,7 @@
-
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.117.0"
-    }
-  }
+provider "azurerm" {
+  features {}
 }
+
 data "azurerm_subscription" "current" {}
 
 resource "random_id" "prefix" {
